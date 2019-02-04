55f32ae6b11c52d5b3f860557b3a4c17c1a052e0
AishSI <aishsi@gmail.com>
2019-02-04 13:29:55 +0700

    Add project files.

---
+++ table-parser.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("table-parser.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("table-parser.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f88143e5-1e1b-4b25-bbcc-81c9bffd9592")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ table-parser.tests/TableParserTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableParser;
+
+namespace table_parser.tests
+{
+	[TestClass]
+	public class TableParserTest
+	{
+		//void TestParseLine(string inputLine, List<string> expectedResult)
+		void TestParseLine(string inputLine, List<string> expectedResult)
+		{
+			List<string> result = FieldsParserTask.ParseLine(inputLine);
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[TestMethod]
+		public void noFields()
+		{
+			TestParseLine(string.Empty, new List<string>{ });
+		}
+
+		[TestMethod]
+		public void oneField()
+		{
+			//Arrange:
+			var inputLine = "hello";
+			//Act:
+			var result = FieldsParserTask.ParseLine(inputLine);
+			//Assert:
+			Assert.AreEqual(new[] { "hello" }, result);
+		}
+	}
+}
+
+//public static void RunTests()
+//{
+	//Test(string.Empty, new string[0]); 									//	Нет полей
+	//Test("hello", new[] { "hello"});										//	Одно поле
+	//Test("hello world", new[] { "hello", "world" });						//	Больше одного поля  
+	//																		//	Разделитель длиной в один пробел																			
+	//Test("hello  world", new[] { "hello", "world" });						//	Разделитель длиной >1 пробела//
+	//Test("\"bcd ef\"", new[] { "bcd ef" });								//	Пробел внутри кавычек
+	//Test("a \"bcd\"", new[] { "a", "bcd"});								//	Поле в кавычках после простого поля
+	//Test("\"bcd\" a", new[] { "bcd", "a"});								//	Простое поле после поля в кавычках
+	//Test("\"a\"\"b\"", new[] { "a", "b"});								//	Разделитель без пробелов
+	//Test(" \"a\"\"b\" ", new[] { "a", "b"});								//	Пробелы в начале или в конце строки
+	//Test("\\\"a", new[] { "\\", "a"});									//	Нет закрывающей кавычки
+	//Test("\"\\\\\"", new[] { "\\"});										//	Экранированный обратный слэш внутри кавычек
+	//																		//	Экранированный обратный слэш перед закрывающей кавычкой
+	//Test("'\\\''",new []{"\'"});											//	Экранированные одинарные кавычки внутри одинарных
+	//Test("\"\\\"\"",new []{"\""});										// 	Экранированные двойные кавычки внутри двойных
+	//Test("\"'a'\"",new []{"'a'"});										//	Одинарные кавычки внутри двойных
+	//Test("\'\"\"\'",new []{"\"\""});										//	Двойные кавычки внутри одинарных
+	//Test("''",new String[]{""});											//	Пустое поле
+	//Test("' ",new String[]{" "});											//	Пробел в конце поля с незакрытой кавычкой 	//
+
+//}
